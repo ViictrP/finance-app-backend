5b638b1a5ad587efd6afc5a392f15c63590b4cb4
Victor <vtrownz@hotmail.com>
2022-07-08 17:22:22 -0300

    removed unnecessary migrations

--- prisma/migrations/20220708192003_added_salary_v3/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Made the column `salary` on table `finance_app_user` required. This step will fail if there are existing NULL values in that column.
-
-*/
--- AlterTable
-ALTER TABLE "finance_app_user" ALTER COLUMN "salary" SET NOT NULL;
