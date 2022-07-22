8c317250f9ab92497e6069230b850cff0b1bda4a
Victor <vtrownz@hotmail.com>
2022-07-21 23:54:33 -0300

    security changes

--- src/adapters/resetAdapter.ts
+++ src/adapters/resetAdapter.ts
@@ -1,9 +1,17 @@
 import { Request, Response } from 'express';
 import { log } from '../core/logger/logger';
 import { prisma } from '../infra/prisma';
+import jwt from 'jsonwebtoken';
 
 const resetAdapter = async (req: Request, res: Response) => {
   log('[resetAdapter]: reset data request received');
+  const { headers } = req;
+  const token = headers[process.env.TOKEN_HEADER_KEY as string] as string;
+  log('[resetAdapter]: extracting user data from access token');
+  const { email } = jwt.verify(token, process.env.JWT_SECRET as string) as any;
+  if (email !== 'admin@financeapp.com') {
+    res.status(403).json({ message: 'not permitted' });
+  }
   await prisma.transaction.deleteMany();
   await prisma.invoice.deleteMany();
   await prisma.creditCard.deleteMany();
