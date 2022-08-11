04cc998eb8a4ebfcae86b93f27fadae04e27cee2
Victor <victor.prado@capco.com>
2022-08-10 23:52:46 -0300

    StatisticsPage.tsx and balances endpoint updated to fix business logic

--- src/core/usecases/getBalanceUseCase.ts
+++ src/core/usecases/getBalanceUseCase.ts
@@ -16,23 +16,9 @@ const getBalanceUseCase = async (filter: TransactionFilter, repository: UserRepo
   const { user, month, year } = filter;
   log(`[getBalanceUseCase]: getting the transactions of ${month}/${year}`);
   const profile = await repository.get(user, month, year);
-  log(`[getBalanceUseCase]: calculating the balance`);
-  const debitAmount = profile!.transactions.reduce((sum, current) => sum + Number(current.amount), 0);
-  const creditCardsAmount = profile!.creditCards.reduce((sum, current) => {
-    const transactions = current.invoices
-      .map(invoice => invoice.transactions)
-      .reduce((sum, current) => sum.concat(current), []);
-    const amount = transactions.reduce((sum, current) => {
-      return sum + Number(current.amount);
-    }, 0);
-    return sum + Number(amount);
-  }, 0);
-
-  const expensesAmount = debitAmount + creditCardsAmount;
-  const balanceAmount = Number(profile!.salary) - expensesAmount;
   return {
-    expensesTotalAmount: expensesAmount,
-    availableAmount: balanceAmount,
+    creditCards: profile!.creditCards,
+    transactions: profile!.transactions,
   }
 };
 
