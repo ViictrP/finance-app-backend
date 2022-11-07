2fe68dfec10132bad2cc467cedb2f56a031cc5c0
Victor Prado <victor.prado@arco-one.com>
2022-11-07 18:08:02 -0300

    fixing balance screen and others improvements

--- src/infra/userPrismaRepository.ts
+++ src/infra/userPrismaRepository.ts
@@ -2,38 +2,37 @@ import { User } from '../core/entities';
 import { prisma } from './prisma';
 import { MONTHS } from '../core/enums/month.enum';
 
-const monthStart = new Date();
-const monthEnd = new Date();
-monthStart.setDate(1);
-monthEnd.setDate(1);
-monthEnd.setMonth(monthStart.getMonth() + 1);
-console.log('TESTE AQUI ZÉ: ', monthStart, monthEnd);
-
-
-const getIncludes = (month?: string, year?: number) => ({
-  creditCards: {
-    include: {
-      invoices: {
-        where: {
-          month: month ?? MONTHS[monthStart.getMonth()],
-          year: year ?? monthStart.getFullYear()
-        },
-        include: {
-          transactions: true,
+const getIncludes = (month?: string, year?: number) => {
+  const monthStart = new Date();
+  const monthEnd = new Date();
+  monthStart.setDate(1);
+  monthEnd.setDate(1);
+  monthEnd.setMonth(monthStart.getMonth() + 1);
+  return {
+    creditCards: {
+      include: {
+        invoices: {
+          where: {
+            month: month ?? MONTHS[monthStart.getMonth()],
+            year: year ?? monthStart.getFullYear()
+          },
+          include: {
+            transactions: true,
+          },
         },
       },
     },
-  },
-  transactions: {
-    where: {
-      invoice: null,
-      date: {
-        gte: month && year ? new Date(year, MONTHS.indexOf(month), 1).toISOString() : monthStart.toISOString(),
-        lte: month && year ? new Date(year, MONTHS.indexOf(month), 31).toISOString() : monthEnd.toISOString()
+    transactions: {
+      where: {
+        invoice: null,
+        date: {
+          gte: month && year ? new Date(year, MONTHS.indexOf(month), 1).toISOString() : monthStart.toISOString(),
+          lte: month && year ? new Date(year, MONTHS.indexOf(month), 31).toISOString() : monthEnd.toISOString()
+        }
       }
-    }
-  },
-});
+    },
+  };
+};
 
 const create = (newUser: User) => {
   return prisma.user.create({
