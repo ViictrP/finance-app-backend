f501ec80bfe61707f7ea979a4adaa7b4405fc21b
Victor <victor.prado@arc-one.com>
2022-12-06 00:05:14 -0300

    creating recurring expense feature

--- src/infra/index.ts
+++ src/infra/index.ts
@@ -2,10 +2,12 @@ import userPrismaRepository from './userPrismaRepository';
 import creditCardPrismaRepository from './creditCardPrismaRepository';
 import transactionPrismaRepository from './transactionPrismaRepository';
 import invoicePrismaRepository from './invoicePrismaRepository';
+import recurringExpensePrismaRepository from './recurringExpensePrismaRepository';
 
 export {
   userPrismaRepository,
   creditCardPrismaRepository,
   transactionPrismaRepository,
-  invoicePrismaRepository
+  invoicePrismaRepository,
+  recurringExpensePrismaRepository
 }
