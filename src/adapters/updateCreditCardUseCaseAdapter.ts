15f7be9aef6d2831e4857ce7cf7cc949d0c0e73c
Victor <victor.prado@arc-one.com>
2023-01-16 14:11:15 -0300

    allowing to update the number ad invoice closing day

--- src/adapters/updateCreditCardUseCaseAdapter.ts
+++ src/adapters/updateCreditCardUseCaseAdapter.ts
@@ -7,6 +7,7 @@ const updateCreditCardUseCaseAdapter = async (req: Request, res: Response) => {
   try {
     const { body, params } = req;
     body.id = params.id;
+    body.user = res.locals.user;
     log('[updateCreditCardUseCaseAdapter]: update credit card request received with body {}', body);
     const updatedCreditCard = await updateCreditCardUseCase(body, creditCardPrismaRepository as any);
     log(`[updateCreditCardUseCaseAdapter]: credit card data was updated [id]: ${updatedCreditCard.id}`);
