17789a44da7acbaceba0cb76a4dc4bf77a0691d0
Victor <vtrownz@hotmail.com>
2023-07-01 00:31:17 -0300

    Adding hour to the logs

--- src/server.ts
+++ src/server.ts
@@ -20,5 +20,5 @@ app.use(routes);
 
 const port = process.env.PORT || 3333;
 app.listen(port, () => {
-  console.log(`[server]: HTTP server running at port ${port}`);
+  log(`[server]: HTTP server running at port ${port}`);
 });
