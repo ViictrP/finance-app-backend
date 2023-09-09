0ebb78da22214589ea0dcf81602c75dda542ab5c
Victor <vtrownz@hotmail.com>
2023-09-09 02:36:22 -0300

    Error handling and unit test coverage

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import routes from './routes';
 import { log } from './core/logger/logger';
-import errorHandler from './adapters/handlers/error.handler';
+import { errorHandler } from './adapters/handlers';
 
 log('[server]: Creating the server');
 const app = express();
@@ -10,7 +10,7 @@ const app = express();
 log('[server]: Configuring CORS');
 app.use(cors({
   origin: process.env.FRONT_HOST,
-  methods: ['GET','HEAD','PUT','PATCH','POST','DELETE']
+  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
 }));
 
 log('[server]: Configuring the server');
