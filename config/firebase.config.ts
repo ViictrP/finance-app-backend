5ca2d4c0e5f34405074d1dcff8b8f142eadd6f1a
Victor <vtrownz@hotmail.com>
2023-10-15 23:37:28 -0300

    Fixing firebase private key

--- config/firebase.config.ts
+++ config/firebase.config.ts
@@ -1,7 +1,14 @@
 import admin from 'firebase-admin';
 
 admin.initializeApp({
-  credential: admin.credential.cert(require('./firebase-private-key.json'))
+  credential: admin.credential.cert(
+    JSON.parse(
+      Buffer.from(
+        process.env.FIREBASE_SERVICE_ACCOUNT as string,
+        'base64'
+      ).toString()
+    )
+  )
 });
 
 export default admin;
